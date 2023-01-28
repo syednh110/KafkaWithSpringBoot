d8a1d60ce7459cb59b8360043a72d93eab973929
syednh110 <syednh110@gmail.com>
2023-01-28 23:02:29 +0530

    Config update

--- src/main/java/com/kafka/demo/Config/KafkaTopicConfig.java
+++ src/main/java/com/kafka/demo/Config/KafkaTopicConfig.java
@@ -2,19 +2,26 @@ package com.kafka.demo.Config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configurable
 public class KafkaTopicConfig {
 
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    @Value("${spring.kafka.topic-json.name}")
+    private String jsonTopicName;
+
     @Bean
     public NewTopic kafkaNewTopic(){
-        return TopicBuilder.name("javatopic").build();
+        return TopicBuilder.name(topicName).build();
     }
 
     @Bean
     public NewTopic kafkaNewJsonTopic(){
-        return TopicBuilder.name("javatopic_json").build();
+        return TopicBuilder.name(jsonTopicName).build();
     }
 }
