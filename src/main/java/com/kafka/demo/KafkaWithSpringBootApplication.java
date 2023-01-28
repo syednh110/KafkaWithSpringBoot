98c149ef8947fd06b70843e7d10542322ed0c3a5
syednh110 <syednh110@gmail.com>
2023-01-28 17:13:20 +0530

    Spring boot with kafka Implementation

---
+++ src/main/java/com/kafka/demo/KafkaWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWithSpringBootApplication.class, args);
+	}
+
+}
